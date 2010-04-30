a5ffe5b2686c4327cc95a09978020a22be2a3cc5
Steen Larsen <sla@Macintosh.local>
2010-04-30 15:33:59 +0200

    first

---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject clotest "1.0.0-SNAPSHOT"
+  :description "Clojure test projekt"
+  :dependencies
+    [[org.clojure/clojure "1.1.0"]
+     [org.clojure/clojure-contrib "1.1.0"]
+     [compojure "0.4.0-SNAPSHOT"]
+     [hiccup "0.2.3"]
+     [ring/ring-core "0.2.0"]
+     [ring/ring-jetty-adapter "0.2.0"]
+     [org.clojars.the-kenny/clojure-couchdb "0.2.1"]]
+  :dev-dependencies
+    [[ring/ring-devel "0.2.0"]]
+  :main helloworld1)
