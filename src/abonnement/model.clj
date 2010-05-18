3cf818b90323c8bc7a4dd1e3f783198eadbe2dea
Steen Larsen <sla@Macintosh.local>
2010-05-18 19:23:52 +0200

    added betalings-aftale

--- src/abonnement/model.clj
+++ src/abonnement/model.clj
@@ -1,5 +1,9 @@
 (ns abonnement.model)
 
-(defstruct abonnement :id :juridisk :faktureringsPeriode :leveringer :pris :rabat)
+(defstruct abonnement :id :juridisk :leverings-aftaler :betalings-aftaler)
 
-(defstruct leverings-aftale :abonnement-id :produkt-id :leveringsPeriode :forbruger :betaler)
+(defstruct leverings-aftale :abonnement-id :produkt-id :leveringsPeriode :forbruger)
+
+(defstruct betalings-aftale :abonnement-id :pris :rabat :faktureringsPeriode :betaler)
+
+
