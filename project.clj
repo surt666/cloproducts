5ee29041b2256dc733fe91f5ec81e2648ed57f1c
Steen Larsen <sla@Macintosh.local>
2010-05-21 08:41:25 +0200

    added enlive

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
      [ring/ring-core "0.2.0"]
      [ring/ring-jetty-adapter "0.2.0"]
      [org.clojars.the-kenny/clojure-couchdb "0.2.1"]
-     [clj-time "0.1.0-SNAPSHOT"]]
+     [clj-time "0.1.0-SNAPSHOT"]
+     [enlive "1.0.0-SNAPSHOT"]]
   :dev-dependencies
     [[ring/ring-devel "0.2.0"]]
   :main helloworld1)
