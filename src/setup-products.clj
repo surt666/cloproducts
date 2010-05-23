e8a332f6dc1fe045aa8896435aca162614133e33
Steen Larsen <sla@Macintosh.local>
2010-05-23 14:41:35 +0200

    added kab pricebook

--- src/setup-products.clj
+++ src/setup-products.clj
@@ -20,7 +20,7 @@
 
 (defn create-pricebooks []
   (let [prices [(struct price 1101001 89.00 2.00 1.00 7.00 0 0 99.00)
-                (struct price 1101032 119.00 2.00 1.00 7.00 0 0 119.00)
+                (struct price 1101032 119.00 2.00 1.00 7.00 0 0 129.00)
                 (struct price 1101003 179.00 2.00 1.00 7.00 0 0 189.00)
                 (struct price 1301001 99.00 0 0 0 0 0 99.00)
                 (struct price 1301002 129.00 0 0 0 0 0 129.00)
@@ -32,5 +32,10 @@
                 (struct price 1321003 699.00 0 0 0 0 0 699.00)
                 (struct price 1321003 699.00 0 0 0 0  0699.00)]]
     (let [pricebook (struct pricebook "YouSee" prices)]
+      (create-pricebook pricebook)))
+  (let [prices [(struct price 1101001 74.00 2.00 1.00 7.00 0 0 84.00)
+                (struct price 1101032 109.00 2.00 1.00 7.00 0 0 119.00)
+                (struct price 1101003 159.00 2.00 1.00 7.00 0 0 169.00)]]
+    (let [pricebook (struct pricebook "KAB" prices)]
       (create-pricebook pricebook))))
 
