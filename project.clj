1d7cbb7af2c86a5107450128f31d0e4228990a24
Steen Larsen <steen666@gmail.com>
2010-06-16 20:01:33 +0200

    patched

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
     [[org.clojure/clojure "1.1.0"]
      [org.clojure/clojure-contrib "1.1.0"]
      [compojure "0.4.0-RC3"]
-     [hiccup "0.2.4"]
+     [hiccup "0.2.5"]
      [ring/ring-core "0.2.0"]
      [ring/ring-jetty-adapter "0.2.0"]
      [org.clojars.the-kenny/clojure-couchdb "0.2.1"]
@@ -14,5 +14,6 @@
   :dev-dependencies
     [[ring/ring-devel "0.2.0"]
      [swank-clojure "1.1.0"]
-     [leiningen/lein-swank "1.1.0"]]
+     [leiningen/lein-swank "1.1.0"]
+     [uk.org.alienscience/leiningen-war "0.0.3"]]
   :main routes)
